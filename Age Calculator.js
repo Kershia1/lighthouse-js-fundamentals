4ded1f8a278541b4a873ab1a6cbafc8f870daf6f
Kershia1 <insatiable.meerkat@gmail.com>
2025-05-20 16:24:00 -0700

    typo fixed

--- Age Calculator.js
+++ Age Calculator.js
@@ -14,6 +14,13 @@
   "Suzie is 32 years old."; 
 */
 
+const ageCalculator = (name, yearOfBirth, currentYear) => {
+
+  const age =  currentYear - yearOfBirth;
+  return `${name} is ${age} years old.`;   
+};
+
+console.log(ageCalculator("Suzie", 1983, 2015));
 
 /*
 
