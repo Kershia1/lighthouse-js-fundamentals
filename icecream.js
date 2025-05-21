87bd9ed4643fadbb893ec91e23d223dd4e1af4ff
Kershia1 <insatiable.meerkat@gmail.com>
2025-05-21 06:53:18 -0700

    fix logic error

--- icecream.js
+++ icecream.js
@@ -5,6 +5,6 @@ console.log(iceCreamFlavours);
 // Your code below here...
 const count = iceCreamFlavours.push( 'root beer');
 console.log(iceCreamFlavours); // new array with root beer added at the end 
-console.log(iceCreamFlavours.[0]); // chocolate
-console.log(iceCreamFlavours.[6]); // rootbeer
+console.log(iceCreamFlavours[0]); // chocolate
+console.log(iceCreamFlavours[6]); // rootbeer
 console.log(iceCreamFlavours.length);
