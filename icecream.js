d12472952834cdbc9639ed5fca4d183cfc8d446a
Kershia1 <insatiable.meerkat@gmail.com>
2025-05-21 07:05:13 -0700

    FIX log return value for last element

--- icecream.js
+++ icecream.js
@@ -1,10 +1,12 @@
 // Do not modify these first two lines
 const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+
 console.log(iceCreamFlavours);
 
 // Your code below here...
-const count = iceCreamFlavours.push( 'root beer');
+iceCreamFlavours.push( 'root beer');
 console.log(iceCreamFlavours); // new array with root beer added at the end 
 console.log(iceCreamFlavours[0]); // chocolate
-console.log(iceCreamFlavours[6]); // rootbeer
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]);
+//console.log(iceCreamFlavours.length -1); // rootbeer // not to self not python think in JS
 console.log(iceCreamFlavours.length);
