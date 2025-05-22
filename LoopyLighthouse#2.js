2ae3113954eb2263e63a65f345022cf3cf58803a
Kershia1 <insatiable.meerkat@gmail.com>
2025-05-22 16:17:57 -0700

    begin refactor

--- LoopyLighthouse#2.js
+++ LoopyLighthouse#2.js
@@ -1,18 +1,35 @@
-function loopyLighthouse() { // function expression  if I do ([]) empty destructuring error recieved
-let x = ["Sambro"];
-let y = [];
-let z = [];
-for(let x = 0;  x <= 88; x ++) { // outer loop to initialize test-condition 
-  }
-  if ( x === % 3); // 1st statement  
-  console.log(x);
-  }
-    for ( let y = 0; y <= 88; y ++) // 2ed statement 
-    if (y === % 3);
-    console.log(y); {
-      for (let z = 0; z <=88; z ++) // 3ed statement 
-      if (z === % 7);
-    } 
+/* Build upon previous loopyLighthouse function to take in  
+Range: array of two numbers representing the start and end values for the loop
+Multiples: array of two numbers representing the multiples you want to replace with words
+Words: array of two strings representing the words that will replace the multiples
+
+Example Input:
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
+
+Desired Output:
+loopyLighthouse function log to the console all the numbers from 15 to 90, except replacing multiples of 2 with "Batty", multiples of 5 with "Beacon", and multiples of 2 and 5 with "BattyBeacon"
+
+*/
+
+
+
+
+
+// function loopyLighthouse() { // function expression  if I do ([]) empty destructuring error recieved
+// let x = ["Sambro"];
+// let y = [];
+// let z = [];
+// for(let x = 0;  x <= 88; x ++) { // outer loop to initialize test-condition 
+//   }
+//   if ( x === % 3); // 1st statement  
+//   console.log(x);
+//   }
+//     for ( let y = 0; y <= 88; y ++) // 2ed statement 
+//     if (y === % 3);
+//     console.log(y); {
+//       for (let z = 0; z <=88; z ++) // 3ed statement 
+//       if (z === % 7);
+//     } 
 
 
 
