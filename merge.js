9a84705d306007ba128a8645833e75c3f945129e
Kershia1 <insatiable.meerkat@gmail.com>
2025-05-26 11:45:50 -0700

    ADD tests

--- merge.js
+++ merge.js
@@ -12,4 +12,18 @@
   return result;
 }
 
+Desired output:
+merge([ 4, 5, 6 ], [ 1, 2, 3, 4 ]); =>	sortedMerge[ 1, 2, 3, 4, 4, 5, 6 ]
+
  */
+
+function concat (array1, array2) {
+  let result = array1.concat(array2);
+  return result;
+}
+
+
+//provided logs for testing
+console.log(merge([4, 5, 6], [1, 2, 3, 4]), "=?", [1, 2, 3, 4, 4, 5, 6]);
+console.log(merge([4], [2, 5, 8]), "=?", [2, 4, 5, 8]);
+console.log(merge([1, 2, 6], []), "=?", [1, 2, 6]);
