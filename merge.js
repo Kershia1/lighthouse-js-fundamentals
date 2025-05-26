13b435d1c2be4239a7458ab0516eb84d1e7fd048
Kershia1 <insatiable.meerkat@gmail.com>
2025-05-26 11:43:36 -0700

    ADD requirments

---
+++ merge.js
@@ -0,0 +1,15 @@
+/**
+ Alter concate array 
+ to merge per-sorted nested arrays provided by researchs
+
+ return a shallow copy, ingore sparse sections, return on 1st level
+
+
+ original logic to be changed: 
+
+ function concat (array1, array2) {
+  let result = array1.concat(array2);
+  return result;
+}
+
+ */
