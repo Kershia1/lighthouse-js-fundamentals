2d121fbe9d0eb2ddbd26e2f421de26844e2d6750
Kershia1 <insatiable.meerkat@gmail.com>
2025-05-27 14:43:47 -0700

    pseudocode

---
+++ kata10.js
@@ -0,0 +1,18 @@
+/**
+ * create a multiplcation table that will stop at at highest given numerical value
+ * 
+ * bonus filter for not numbers return null then continue.
+ * 
+ */
+
+const multiplicationTable = function (maxValue) {
+  //rule out non integers inmmediately 
+  //for loop, stop at maxValue
+  // how many times can input be divisable into maxValue
+  // edge case return divisable integer as only a whole 
+  // Your code here
+};
+
+console.log(multiplicationTable(1));
+console.log(multiplicationTable(5));
+console.log(multiplicationTable(10));
