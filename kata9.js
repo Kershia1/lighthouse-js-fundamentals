b08300ffd19dbf3b0cf85d525a1eb71838c3e43e
Kershia1 <insatiable.meerkat@gmail.com>
2025-05-27 13:14:29 -0700

    ADD more logic

--- kata9.js
+++ kata9.js
@@ -6,8 +6,23 @@
  * thisIsAString
 loopyLighthouse
 supercalifragalisticexpialidocious
+
+I could use regex, or do extra with clear to read code?
+
+
  */
 
 const camelCase = function (input) {
 
+  for (let i = 0; i >= 0; i++) {
+    //look for an empty space
+    //remove empty space, next element toUppercase
+    //concatenat and return shallow copy.
+
+  }
+
 };
+
+console.log(camelCase("this is a string"));
+console.log(camelCase("loopy lighthouse"));
+console.log(camelCase("supercalifragalisticexpialidocious"));
