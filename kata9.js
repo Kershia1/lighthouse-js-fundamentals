9cc959bf7bb3f6e81c75c595f42c829f895e071a
Kershia1 <insatiable.meerkat@gmail.com>
2025-05-27 13:09:53 -0700

    start kata

---
+++ kata9.js
@@ -0,0 +1,13 @@
+/**
+ * Convert a string to camel case 
+ * 
+ * Desired Output: 
+ * 
+ * thisIsAString
+loopyLighthouse
+supercalifragalisticexpialidocious
+ */
+
+const camelCase = function (input) {
+
+};
